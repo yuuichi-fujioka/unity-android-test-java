1e9e6583f36510b25ff7904f01ffb6360e4b00b1
Yuuichi Fujioka <fujioka.yuuichi@gmail.com>
2016-02-04 20:27:42 +0900

    init

---
+++ src/main/java/jp/test/lib/Hoge.java
@@ -0,0 +1,13 @@
+package jp.test.lib;
+
+class Hoge {
+    public static int Hello() {
+        return 9999;
+    }
+
+    public static int Hello2(char[] data) {
+        return 9898;
+    }
+}
+
+
